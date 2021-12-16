2c712e4f15a067ffd6efe82e6d5edf5f47c88924
qkrtkdwns3410 <qkrtkdwns3410@naver.com>
2021-12-16 23:02:54 +0900

    타임리프로 게시판 이동 구현하기

--- src/main/java/kr/co/psj/controller/BoardController.java
+++ src/main/java/kr/co/psj/controller/BoardController.java
@@ -5,9 +5,9 @@ import kr.co.psj.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 
 /*
@@ -29,9 +29,21 @@ public class BoardController {
     }
 
     @GetMapping("/form")
-    public String form(Model model) {
+    public String form(Model model, @RequestParam(required = false) Long id) {
+        if (id == null) {
+            model.addAttribute("board", new Board());
+        } else {
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
 
+        }
         return "/board/form";
+    }
+
+    @PostMapping("/form")
+    public String greetingSubmit(@ModelAttribute Board board) {
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 }
 
